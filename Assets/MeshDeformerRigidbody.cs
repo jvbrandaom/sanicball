269d76abfe0247fae6a695810c82541f8bd00860
lucasmontec <lucas.mcarvalhaes@gmail.com>
2017-01-24 20:08:31 -0200

    Initial commit

---
+++ Assets/MeshDeformerRigidbody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshDeformer))]
+public class MeshDeformerRigidbody : MonoBehaviour {
+
+    private MeshDeformer deformer;
+
+	void Start () {
+        deformer = GetComponent<MeshDeformer>();
+    }
+	
+	void OnCollisionEnter(Collision col) {
+        if (deformer) {
+            ContactPoint hit = col.contacts[0];
+
+            deformer.AddDeformingForce(hit.point, col.relativeVelocity.magnitude*6f);
+        }
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Camera mainCamera;
+
+    [SerializeField]
+    private float speed = 250f;
+
+    private Rigidbody playerBody;
+
+	void Start () {
+        if (player) {
+            playerBody = player.GetComponent<Rigidbody>();
+        }else {
+            Debug.LogError("Missing player object for player controller!");
+        }
+
+        if (!playerBody) {
+            Debug.LogError("Missing rigidbody object for player object!");
+        }
+	}
+	
+	void Update () {
+        if (Input.GetAxis("Vertical") > 0f) {
+            playerBody.AddForce(mainCamera.transform.forward * speed * Time.deltaTime, ForceMode.Force);
+        }
+	}
+}
