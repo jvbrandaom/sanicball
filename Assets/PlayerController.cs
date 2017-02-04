45b1adcc9692caee0d7c1efe23bc130cf3ab0272
João Brandão <joao.brandao@vistajet.com>
2017-02-04 16:41:34 -0200

    Adds stamina bar

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,19 +9,48 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private float speed = 250f;
+    [SerializeField]
+    private ForceMode force = ForceMode.Force;
     private bool isOnGround;
     [SerializeField]
     [Tooltip("This is the ground inclination. Default is 0.6")]
     private float groundSlope = 0.6f;
     private Rigidbody playerBody;
+    private Vector3 forward;
+    private float stamina = 1;
+    public float Stamina { get { return stamina; } }
+    private bool running = false;
+    [SerializeField]
+    private float runningSpeed = 2;
+    public static PlayerController localPlayer;
 
 	void Start () {
         playerBody = GetComponent<Rigidbody>();
+        localPlayer = this;
 	}
 	
 	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+        {
+            Debug.Log(stamina);
+            running = true;
+            stamina -= 0.01f;
+        }
+        if (!Input.GetKey(KeyCode.LeftShift) && stamina <= 1)
+        {
+            running = false;
+            stamina += 0.05f;
+        }
+
         if (Input.GetAxis("Vertical") > 0f && isOnGround) {
-            playerBody.AddForce(mainCamera.transform.forward * speed * Time.deltaTime, ForceMode.Force);
+           
+            forward = Vector3.ProjectOnPlane(mainCamera.transform.forward, Vector3.up);
+            if (running) {
+                playerBody.AddForce(forward * runningSpeed * speed * Time.deltaTime, force);
+            }
+            else {
+                playerBody.AddForce(forward * speed * Time.deltaTime, force);
+            }
             isOnGround = false;
         }
 	}
