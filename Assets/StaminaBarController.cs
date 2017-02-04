4fc79ba272f952fe831aafeb4997a43fc1fc52db
João Brandão <joao.brandao@vistajet.com>
2017-02-04 19:18:11 -0200

    Fixes shortcut to track end

---
+++ Assets/StaminaBarController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBarController : MonoBehaviour {
+
+    [SerializeField]
+    private Image fillImage;
+		
+	// Update is called once per frame
+	void Update () {
+        fillImage.fillAmount = Mathf.Clamp(PlayerController.localPlayer.Stamina, 0, 1);
+	}
+}
