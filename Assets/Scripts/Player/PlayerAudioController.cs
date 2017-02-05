f77e4204fe23b8c1b020d9f1d12c73c5cc172052
lucasmontec <lucas.mcarvalhaes@gmail.com>
2017-02-05 01:08:40 -0200

    Added new sfx

--- Assets/Scripts/Player/PlayerAudioController.cs
+++ Assets/Scripts/Player/PlayerAudioController.cs
@@ -10,17 +10,28 @@ public class PlayerAudioController : MonoBehaviour {
     [SerializeField]
     private AudioClip bounceClip;
 
+    private float mag = 0f;
+    private float lastEmited = 0f;
+    [SerializeField]
+    [Tooltip("Delay between sounds emited")]
+    private float effectDelay = 0.3f;
+
 	void Start () {
         source = GetComponent<AudioSource>();
         source.loop = false;
         source.playOnAwake = false;
+        //Debug.Log("Started");
     }
 
-    void OnCollsionEnter(Collision collisionInfo) {
-        Debug.Log("Collided");
-        Debug.Log(collisionInfo.relativeVelocity.magnitude);
-        if (bounceClip) {
-            //source.pitch = collisionInfo.relativeVelocity.magnitude;
+    void OnCollisionEnter(Collision collisionInfo) {
+        //Debug.Log("Collided");
+        //Debug.Log(collisionInfo.relativeVelocity.magnitude);
+        //collisionInfo.relativeVelocity.magnitude usually max at 20f
+        mag = collisionInfo.relativeVelocity.magnitude;
+        if (bounceClip && mag > 1f && Time.realtimeSinceStartup-lastEmited > effectDelay) {
+            lastEmited = Time.realtimeSinceStartup;
+            source.pitch = Mathf.Clamp(Random.value/2f + (mag / 8f), 0f, 2f);
+            source.volume = Mathf.Clamp01(0.1f + (mag / 22f));
             source.PlayOneShot(bounceClip);
         }
     }
